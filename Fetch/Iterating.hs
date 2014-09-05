d73d50f9b9aace8760261a2685db27ea26c56f05
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2014-09-05 21:18:52 +0200

    Unified failure into ExceptT in Fetch.Iterating.
    Related to issue #1 and #2.
    See commit c52cedc.

--- Fetch/Iterating.hs
+++ Fetch/Iterating.hs
@@ -4,32 +4,25 @@ import Control.Monad
 import Control.Monad.Loops
 import Data.Maybe (catMaybes)
 import Data.Text (unpack, Text)
-import Network.HTTP.Client (defaultManagerSettings)
 import Network.HTTP.Conduit hiding (path, withManager)
 import Text.HTML.DOM (parseLBS)
 import Text.XML.Cursor
 
 import Fetch
 
-type CrawlIterator = (Manager, Maybe URL) -> IO (Maybe (Maybe URL, (Manager, Maybe URL)))
+type CrawlIterator = Maybe URL -> ErrorIO (Maybe (Maybe URL, Maybe URL))
 type TextExtractor = Cursor -> Maybe Text
 
-fetchIterate :: TextExtractor -> TextExtractor -> CrawlIterator
-fetchIterate _ _ (_,Nothing) = return Nothing
-fetchIterate next item (m,Just path) = 
-   do (m', f) <- withManager download (Just m) path
-      case f of Left _ -> return Nothing
-                Right f' ->
-                   do let doc = parseLBS f'
-                          nextLink = runXPath doc next
-                          itemLink = runXPath doc item
-                      return $! Just (liftM unpack itemLink, (m', liftM unpack nextLink))
+fetchIterate :: Manager -> TextExtractor -> TextExtractor -> CrawlIterator
+fetchIterate _ _ _ Nothing = return Nothing
+fetchIterate m next item (Just path) = 
+   do res <- download m path
+      let doc = parseLBS res
+          nextLink = runXPath doc next
+          itemLink = runXPath doc item
+      return $! Just (liftM unpack itemLink, liftM unpack nextLink)
 
-fetchList :: URL -> CrawlIterator -> IO [URL]
+fetchList :: URL -> CrawlIterator -> ErrorIO [URL]
 fetchList start iter =
-   do m <- newManager defaultManagerSettings
-      list <- unfoldrM (status >=> iter) (m, Just start) 
+   do list <- unfoldrM iter (Just start) 
       return $! catMaybes list
-   where
-      status (m,Nothing) = return (m,Nothing) 
-      status (m,Just path) = putStrLn (path ++ " retrieved!") >> return (m,Just path)
