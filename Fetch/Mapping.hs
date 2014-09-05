271b4c3ea234d091e8d988b619901455d4d73a8c
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2014-09-05 21:33:01 +0200

    Unified failure mechanism in Fetch.Mapping.

--- Fetch/Mapping.hs
+++ Fetch/Mapping.hs
@@ -16,16 +16,14 @@ pictureFetch before after = map (splice before after)
 -- |Executes a series of fetch requests.
 --  At each step, a URL is retrieved and run against
 --  the next function in the list. The result of the
---  last function is returned. In case of error, the function
---  return Nothing.
-urlFetch :: Manager -> URL -> [Document -> Maybe URL] -> IO (Maybe URL)
-urlFetch m = foldM f . Just
-   where f Nothing _ = return Nothing
-         f (Just url) g =
-            do res <- download m url
-               case res of
-                  Left _ -> return Nothing
-                  Right r -> return $ g (parseLBS r)
+--  last function is returned. In case of error, an exception is thrown.
+urlFetch :: Manager -> URL -> [Document -> Maybe URL] -> ErrorIO URL
+urlFetch m = foldM f
+   where f :: URL -> (Document -> Maybe URL) -> ErrorIO URL
+         f url g = do res <- download m url
+                      case g $ parseLBS res of
+                         Nothing -> addError $ DataFindingError $ "Could not find element on '"++url++"'!"
+                         (Just r) -> return r
 
 -- |Concatenates three lists. The third list is put between the
 --  first two.
