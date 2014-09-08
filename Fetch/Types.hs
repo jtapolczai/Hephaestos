8ffdb9ea20b988b362def0574e73517945a12ede
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2014-09-08 17:10:16 +0200

    Renamed NetworkError to NetworkErrorKind, wrapped it.
    The new NetworkError-type now consists of a NetworkErrorKind and a URL
    to identify the source of the error.

--- Fetch/Types.hs
+++ Fetch/Types.hs
@@ -6,12 +6,13 @@ module Fetch.Types (
    InfoExtractor,
    Info,
    NetworkError (..),
+   NetworkErrorKind (..),
    ErrorIO,
    ) where
 
 import Control.Monad.Except
 import Data.Text
-import Text.XML.Cursor
+import Text.XML.HXT.DOM.TypeDefs
 
 -- |A URL.
 type URL = String
@@ -19,30 +20,40 @@ type URL = String
 type HTTPStatus = Int
 
 -- |A function which tries to extract content from a DOM tree.
-type TextExtractor = Cursor -> Maybe Text
+type TextExtractor = XmlTree -> Maybe Text
 -- |A function tries to extract a key-value-pair from a DOM tree.
 --  The first value of the result is the key, the second is the
 --  value and may be absent.
-type InfoExtractor = Cursor -> Info Text Text
+type InfoExtractor = XmlTree -> Info Text Text
 
 -- |Auxiliary information that was extracted from a page
 --  but isn't the primary content.
 type Info k v = (k, Maybe v)
 
--- |The sum type of all network or file errors
---  that occur during fetching URLs or saving files locally.
-data NetworkError = ConnectionError String
-                    | StatusError HTTPStatus String
-                    | FileError String
-                    | FormatError String
-                    | DataFindingError String
+-- |A network error, consisting of a 'NetworkErrorKind' and
+--  a URL indicating the error's source (if any).
+data NetworkError = NetworkError URL NetworkErrorKind
 
 instance Show NetworkError where
+   show (NetworkError url kind) = "Error in '" ++ url ++ "': " ++ show kind
+
+-- |The sum type of all network or file errors
+--  that occur during fetching URLs or saving files locally.
+data NetworkErrorKind = ConnectionError String
+                        | StatusError HTTPStatus String
+                        | FileError String
+                        | FormatError String
+                        | DataFindingError String
+                        | HTMLParsingError
+
+
+instance Show NetworkErrorKind where
    show (ConnectionError m) = m
    show (StatusError s m) = show s ++ ": " ++ m
    show (FileError m) = m
    show (FormatError m) = m
    show (DataFindingError m) = m
+   show (HTMLParsingError) = "Couldn't parse file as HTML!"
 
 -- |An ExceptT-wrapper around IO. The type of all IO actions
 --  in the program.
