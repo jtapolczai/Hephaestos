8159228ab5b3baf90104bf30086dab78a472e0aa
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2014-09-11 17:47:37 +0200

    Added file reading feature for linear comics.
    Comics.List.comics now reads in all comic
    files from a given directory, reducing the dependence on hardcoded
    crawling scripts.
    
    Also wrote Fetch.ErrorHandling.filterErr (incomplete).

--- Comics/List.hs
+++ Comics/List.hs
@@ -1,17 +1,39 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 
 -- |Crawlers for linear webcomics.
 module Comics.List where
 
-import Network.HTTP.Conduit
-import Data.Text
+import Data.Either
+import qualified Data.Map as M
+import Data.Maybe
+import qualified Data.Text as T
+import System.Directory
+import Text.Read (readMaybe)
+
 
 import Comics.LinearComic
-import Fetch
-import Fetch.Iterating
-import Helper.String
-import XPath
+import Fetch.ErrorHandling
+import Fetch.Types
 
+-- |Loads the list of comics from a given directory,
+--  printing out any errors that occur.
+--  This function is fault-tolerant, i.e. skips over any unreadable
+--  scripts.
+comics :: String -> ErrorIO (M.Map T.Text LinearComic)
+comics dir = do contents <- fErr $ getDirectoryContents dir
+                (files,errs) <- filterErr (fErr . doesFileExist) contents
+                mapM_ printError errs
+                res <- mapErr tryRead files
+                mapM_ (mapM_ printError) (lefts res)
+                let res' = map (\(Just v) -> (comicName v, v)) $ filter isJust $ rights res
+                return $ M.fromList res'
+   where
+      fErr = catchIO "File" FileError
+      tryRead fp = do c <- fErr $ readFile fp
+                      return $ readMaybe c
+
+xkcd :: LinearComic
 xkcd =
    LinearComic "XKCD"
                "http://www.xkcd.com/"
@@ -21,6 +43,7 @@ xkcd =
                "(//a[@rel=\"next\"])[1]/@href/text()"
                "(//a[@rel=\"prev\"])[1]/@href/text()"
 
+pennyArcade :: LinearComic
 pennyArcade =
    LinearComic "Penny Arcade"
                "http://www.penny-arcade.com/"
@@ -30,6 +53,7 @@ pennyArcade =
                "(//a[@class=\"btn btnNext\"])[1]/@href/text()"
                "(//a[@class=\"btn btnPrev\"])[1]/@href/text()"
 
+cyanideAndHappiness :: LinearComic
 cyanideAndHappiness =
    LinearComic "Cyanide and Happiness"
                "http://www.explosm.net/"
--- Fetch/ErrorHandling.hs
+++ Fetch/ErrorHandling.hs
@@ -8,6 +8,7 @@ import Control.Exception (catches, IOException, Handler(..))
 import Control.Monad
 import Control.Monad.Except
 import Data.Either
+import Data.Either.Unwrap
 import Data.Monoid
 
 import Fetch.Types
@@ -63,13 +64,25 @@ guardErr :: MonadError e m => Bool -> e -> m ()
 guardErr True = throwError
 guardErr False = const $ return ()
 
--- |Version of @mapM_@ which collects all the errors which occur.
+-- |Version of 'mapM_' which collects all the errors which occur.
 mapErr_ :: (Monoid e, MonadError e m) => (a -> m b) -> [a] -> m e
 mapErr_ f = mapErr f >=> collectErrors
 
--- |Version of @mapM@ which returns either the result of the function
+-- |Version of 'mapM' which returns either the result of the function
 --  or the error which occurred during its evaluation.
-mapErr :: (Monoid e, MonadError e m) => (a -> m b) -> [a] -> m [Either e b]
+mapErr :: MonadError e m => (a -> m b) -> [a] -> m [Either e b]
 mapErr _ [] = return []
 mapErr f (x:xs) = liftM2 (:) (liftM Right (f x) `catchError` (return . Left))
                              (mapErr f xs)
+
+-- |Version of 'filterM' which collects all the errors encountered.
+--  If @f x = False@ or @f x@ throws an error (for any x in the list),
+--  it is filtered out.
+filterErr :: (Monoid e, MonadError e m) => (a -> m Bool) -> [a] -> m ([a],e)
+filterErr _ [] = return ([],mempty)
+filterErr f (x:xs) = do pr <- liftM Right (f x)
+                        let e' = if isLeft pr then fromLeft pr else mempty
+                        (ys,es) <- filterErr f xs
+                        let ys' = if isRight pr && fromRight pr then x : ys
+                                                                else ys
+                        return $ (ys',e' `mconcat` es)
