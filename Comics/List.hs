09b2fd21269c1b4aafbc63b793864a44477a7656
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2014-09-11 19:06:44 +0200

    Put comics into external scripts.

--- Comics/List.hs
+++ Comics/List.hs
@@ -37,35 +37,3 @@ comics dir = do contents <- fErr $ getDirectoryContents dir
 
                       case readMaybe c of Nothing -> addError $ NetworkError fp $ FileError "Couldn't parse file!"
                                           Just v  -> return v
-
-xkcd :: LinearComic
-xkcd =
-   LinearComic "XKCD"
-               "http://www.xkcd.com/"
-               "http://www.xkcd.com/1/"
-               "http://www.xkcd.com"
-               "//div[@id=\"comic\"]/img/@src/text()"
-               "(//a[@rel=\"next\"])[1]/@href/text()"
-               "(//a[@rel=\"prev\"])[1]/@href/text()"
-
-pennyArcade :: LinearComic
-pennyArcade =
-   LinearComic "Penny Arcade"
-               "http://www.penny-arcade.com/"
-               "http://www.penny-arcade.com/comic/1998/11/18"
-               "http://www.penny-arcade.com/comic"
-               "//*[@id=\"comicFrame\"]//img/@src/text()"
-               "(//a[@class=\"btn btnNext\"])[1]/@href/text()"
-               "(//a[@class=\"btn btnPrev\"])[1]/@href/text()"
-
-cyanideAndHappiness :: LinearComic
-cyanideAndHappiness =
-   LinearComic "Cyanide and Happiness"
-               "http://www.explosm.net/"
-               "http://explosm.net/comics/15/"
-               "http://explosm.net/comics/"
-               "//*[@id=\"maincontent\"]/div[2]/div[1]/img/@src/text()"
-               "//a[@rel=\"next\"]/@href/text()"
-               "//a[@rel=\"prev\"]/@href/text()"
-
-
