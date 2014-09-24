81a98a5b6b133e1ebc6b9315f320be7c229deb23
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2014-09-24 22:27:47 +0200

    Added `>=$>` to Helper.Functor
    As `>$>` is to `>>=`, `>=$>` is to `>=>`.

--- Crawling/Hephaestos/Helper/Functor.hs
+++ Crawling/Hephaestos/Helper/Functor.hs
@@ -14,3 +14,13 @@ infixl 4 >$>
 -- them with returns, as pure '>>=' necessitates, is quite tedious.
 (>$>) :: Functor f => f a -> (a -> b) -> f b
 (>$>) = flip fmap
+
+-- |Application of '>$>' to Kleisli composition 'Control.Monad.>=>'
+--  Use is analogous to that of '>$>', e.g.
+--
+--  @
+--  f :: FilePath -> IO ()
+--  f = (readFile >=$> lines >=$> map length >=> print)
+--  @
+(>=$>) :: Functor f => (a -> f b) -> (b -> c) -> a -> f c
+(>=$>) f g x = f x >$> g
