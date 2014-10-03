96f46df097731eb9569f5b91915c941b90a1cb55
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2014-10-03 09:22:25 +0200

    Added Data.Types.{Injective, Isomorphic}
    The type classes Injective and Iso represent type injectivity and
    isomorphism, respectively. System.{FilePath, Directory}.Generic will be
    based on types which are isomorphic to String instead of a hard
    dependence on Text.

---
+++ Data/Types/Injective.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module Data.Types.Injective where
+
+import qualified Numeric.Natural as N
+import qualified Numeric.Natural.Internal as N (runNatural)
+import qualified Data.Ratio as R
+import qualified Data.Text as T
+import qualified Data.ByteString as B
+
+-- |The class relation between types @a@ and @b@ s.t. @a@ can be injected
+--  into @b@.
+--
+--  The following laws must be fulfilled:
+--  * 'to' is total.
+--  * @x /= y  ==>  to x /= to y@.
+class Injective a b where
+   to :: a -> b
+
+instance Injective a a where
+   to = id
+
+instance Injective T.Text String where to = T.unpack
+instance Injective String T.Text where to = T.pack
+instance Injective N.Natural Integer where to = N.runNatural
+instance Injective Integer R.Rational where to = flip (R.%) 1
