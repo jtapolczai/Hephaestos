8844ff4fd42a517266080b1585e4acd2888ea61f
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2014-10-19 16:51:29 +0200

    Added allImages crawler.

--- Crawling/Hephaestos/Crawlers/Templates.hs
+++ Crawling/Hephaestos/Crawlers/Templates.hs
@@ -3,17 +3,22 @@
 -- |Example Crawlers for a variety of use-cases.
 module Crawling.Hephaestos.Crawlers.Templates (
    fileList,
-   fileList')where
+   fileList',
+   allImages)where
 
 import Control.Arrow
 import Data.Char
+import Data.Functor.Monadic
 import Data.List.Split
+import Data.Maybe (mapMaybe)
 import qualified Data.Text as T
 import Data.Void
+import Numeric.Peano
 
 import Crawling.Hephaestos.Fetch
 import Crawling.Hephaestos.Fetch.Types.Successor
 import Crawling.Hephaestos.Helper.String hiding ((++))
+import Crawling.Hephaestos.XPath
 
 
 -- |Downloads a list of numbered files.
@@ -62,3 +67,14 @@ fileList' num url = fileList range url
 
       range = case e of Nothing -> []
                         Just e' -> [read e'..read e'+num-1]
+
+-- |Retrieves all images on a page.
+--  Only the contents of src-attributes in img-tags count
+--  as images; background images are not.
+allImages :: Successor Void [NetworkError]
+allImages = htmlSuccessor id allImages'
+   where
+      allImages' url doc _ = (images,[])
+         where
+            images = mapMaybe (getText >=$> combineURL url >=$> Blob >=$> voidNode)
+                     $ getXPathLeaves "//img/@src/text()" doc
