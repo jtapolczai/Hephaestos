00463cf36fbaa4602c1e2bbd5df91f8267482a37
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2014-10-27 11:20:20 +0100

    Closes #47.

--- Crawling/Hephaestos/Fetch/Tree.hs
+++ Crawling/Hephaestos/Fetch/Tree.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE GADTs #-}
 
 -- |General tree-crawlers which generate 'MTree's (monad trees)
@@ -50,11 +51,17 @@ import Crawling.Hephaestos.XPath
 --  @l@ is the type of the keys in the leaf nodes and @n@ the type
 --  of the keys in the inner nodes.
 data MTree m n = -- |An internal nodes with a value and children
-                   --  wrapped in a monad.
-                   MNode {nodeContent::n, nodeChildren::m [MTree m n]}
+                 --  wrapped in a monad.
+                 MTree (m (MNode m n))
+
+-- |A node of an 'MTree'.
+data MNode m n = MNode {nodeContent::n, nodeChildren::[MTree m n]}
 
 instance (Functor m) => Functor (MTree m) where
-   fmap f (MNode n ns) = MNode (f n) $ fmap (fmap (fmap f)) ns
+   fmap f (MTree m) = MTree $ fmap (fmap f) m
+
+instance Functor m => Functor (MNode m) where
+   fmap f (MNode n ns) = MNode (f n) (fmap (fmap f) ns)
 
 type FetchTreeArgs = [Manager, (Request -> Request), URL]
 
@@ -62,48 +69,47 @@ type FetchTreeArgs = [Manager, (Request -> Request), URL]
 --  and generates a monadic tree of crawled results.
 --  Only internal nodes (Inner-constructor) will be expanded. All others will be
 --  turned into leaves.
-fetchTree :: Manager -- ^The connection manager.
-          -> Successor SomeException a -- ^Node-expanding function with state @a@.
-          -> (Request -> Request) -- ^Global modifiers to all requests.
-                                  --  Parts of these may be overridden by
-                                  --  the 'Successor' function.
+fetchTree :: forall a. Manager -- ^The connection manager.
+          -> Successor SomeException a
+          -- ^Node-expanding function with state @a@.
+          -> (Request -> Request)
+          -- ^Global modifiers to all requests. Parts of these may be
+          --  overridden by the 'Successor' function.
           -> a -- ^Initial state to be given to the node-expanding function.
           -> URL -- ^The initial URL.
           -> MTree ErrorIO' (SuccessorNode SomeException a)
-             -- ^Resultant tree of crawl results.
+          -- ^Resultant tree of crawl results.
 fetchTree m succ reqF = fetchTreeInner m succ reqF id
-
--- Has an added "reqLocal" parameter that modifies the HTTP request for
--- one call
-fetchTreeInner m succ reqF reqLocal state url = MNode this children
    where
-      recCall (SuccessorNode s r f) = fetchTreeInner m succ reqF f s (fromBlob r)
+      -- Has an added "reqLocal" parameter that modifies the HTTP request for
+      -- one call
+      fetchTreeInner m succ reqF reqLocal state url = MTree results
+         where
+            results :: ErrorIO (MNode ErrorIO' (SuccessorNode SomeException a))
+            results = (do
+               doc <- (download m (reqLocal . reqF) url)
+               -- run the successor function on the fetched document
+               let (leaves, nodes) = succ url doc state
+                   -- Turn non-inner nodes into leaves, since we can't expand them.
+                   (actualNodes, toLeaves) = partition (isInner.nodeRes) nodes
+                   leaves' = map (MTree . leaf) $ leaves ++ toLeaves
+                   -- The recursive call occurs here
+                   nodes' = map recCall actualNodes
 
-      -- The current node.
-      this = (SuccessorNode state (Blob url) (reqLocal . reqF))
+               return $ MNode this $ leaves' ++ nodes')
+               `catchError`
+               (\err -> leaf $ simpleNode state $ Failure url err)
 
-      leaf = flip MNode (return [])
 
-      children = do
-         -- Try to get URL...
-         fetchResult <- (download m (reqLocal . reqF) url >$> Right)
-                        `catchError` (return . Left)
+            -- recursive call to fetchTreeInner
+            recCall (SuccessorNode state result reqMod) =
+               fetchTreeInner m succ reqF reqMod state (fromInner result)
 
-         -- and create a failure node or a list of recursive calls,
-         -- base on success
-         case fetchResult of
-            Left err -> return $ [leaf $ simpleNode state $ Failure url err]
-            Right doc ->
-               do let (leaves, nodes) = succ url doc state
-                      -- Turn non-blob nodes into leaves, since we can't expand them.
-                      (actualNodes, toLeaves) = partition (isInner.nodeRes) nodes
-                      leaves' = map leaf $ leaves ++ toLeaves
-                      -- The recursive call occurs here
-                      mkInput (SuccessorNode s r m) = (m, s, fromInner r)
+            -- The current node.
+            this = (SuccessorNode state (Blob url) (reqLocal . reqF))
 
-                      nodes' = map recCall actualNodes
-
-                  return $ leaves' ++ nodes'
+            -- creates a leaf MNode.
+            leaf = return . flip MNode []
 
 -- |Stateless variant of 'fetchTree'. Convenient for when
 --  the successor function does not need a state.
@@ -131,42 +137,51 @@ packableFetchTree (HCons m (HCons req (HCons url HNil))) cr config state =
    fetchTree m (crawlerFunction cr config) req state url
 
 -- |Extracts all leaves from an 'MTree'. See 'extractFromTree'.
-extractResults :: Monad m => MTree m (SuccessorNode e a) -> m [(SuccessorNode e a)]
+extractResults :: (Functor m, Monad m)
+               => MTree m (SuccessorNode e a) -> m [(SuccessorNode e a)]
 extractResults = extractFromTree (not.isInner.nodeRes) id
 
 -- |Extracts all nodes from an 'MTree' which are 'Blob's.
 --  See 'extractFromTree'
-extractBlobs :: Monad m => MTree m (SuccessorNode e a) -> m [(URL, Request -> Request)]
+extractBlobs :: (Functor m, Monad m)
+             => MTree m (SuccessorNode e a) -> m [(URL, Request -> Request)]
 extractBlobs = extractFromTree (isBlob.nodeRes) (fromBlob . nodeRes &&& nodeReqMod)
 
 -- |Extracts all nodes from an 'MTree' which are 'PlainText's.
 --  See 'extractFromTree'
-extractPlainText :: Monad m => MTree m (SuccessorNode e a) -> m [Text]
+extractPlainText :: (Functor m, Monad m)
+                 => MTree m (SuccessorNode e a) -> m [Text]
 extractPlainText = extractFromTree (isPlainText.nodeRes) (fromPlainText.nodeRes)
 
 -- |Extracts all nodes from an 'MTree' which are 'XmlResult's.
 --  See 'extractFromTree'
-extractXmlResults :: Monad m => MTree m (SuccessorNode e a) -> m [XmlTree]
+extractXmlResults :: (Functor m, Monad m)
+                  => MTree m (SuccessorNode e a) -> m [XmlTree]
 extractXmlResults = extractFromTree (isXmlResult.nodeRes) (fromXmlResult.nodeRes)
 
 -- |Extracts all nodes from an 'MTree' which are 'Failure's.
 --  See 'extractFromTree'
-extractFailures :: Monad m => MTree m (SuccessorNode e a) -> m [SuccessorNode e a]
+extractFailures :: (Functor m, Monad m)
+                => MTree m (SuccessorNode e a) -> m [SuccessorNode e a]
 extractFailures = extractFromTree (isFailure.nodeRes) id
 
 -- |Extracts all nodes from an 'MTree' which are 'Info's. Returns key/value-pairs.
 --  See 'extractFromTree'
-extractInfo :: Monad m => MTree m (SuccessorNode e a) -> m [(Text,Text)]
+extractInfo :: (Functor m, Monad m)
+            => MTree m (SuccessorNode e a) -> m [(Text,Text)]
 extractInfo = extractFromTree (isInfo.nodeRes) ((infoKey &&& infoValue) . nodeRes)
 
 -- |Gets nodes from an 'MTree' from left to right, going breadth-first.
-extractFromTree :: Monad m => (a -> Bool) -- ^Test to determine whether the leaf should be extracted.
+extractFromTree :: (Functor m, Monad m)
+                => (a -> Bool) -- ^Test to determine whether the leaf should be extracted.
                 -> (a -> b) -- ^Function to apply to leaf which passes the test.
                 -> MTree m a -- ^The tree whose results to extract.
                 -> m [b] -- ^Result list.
-extractFromTree test from (MNode a children) = children >>= rec
+extractFromTree test from (MTree m) =  m >>= \(MNode a children) -> rec a children
    where
       -- If the node is a leaf, filter based on the predicate and return.
-      rec [] = return [from a | test a]
+      rec a [] = return [from a | test a]
       -- If there are children, recursively traverse them.
-      rec xs = liftM concat $ mapM (extractFromTree test from) xs
+      rec a xs = mapM (extractFromTree test from) xs
+                 >$> concat
+                 >$> (if test a then ((from a):) else id)
