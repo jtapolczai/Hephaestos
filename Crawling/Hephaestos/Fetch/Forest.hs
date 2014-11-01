7c64418c7af2a5974a3caf26507a41d234c120aa
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2014-11-02 00:43:06 +0100

    Finished Fetch.Forest.

--- Crawling/Hephaestos/Fetch/Forest.hs
+++ Crawling/Hephaestos/Fetch/Forest.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE ConstraintKinds #-}
 
 -- |All-inclusive downloading. This module uses 'Crawling.Hephaestos.Fetch'
 --  and 'Crawling.Hephaestos.Fetch.Tree' as building blocks to provide
@@ -17,6 +19,8 @@ import Control.Foldl (FoldM(..), foldM)
 import qualified Data.Binary as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Collections as Co
+import qualified Data.Collections.BulkInsertable as Co
+import qualified Data.Collections.Instances as Co
 import Data.Char (toLower)
 import Data.Dynamic
 import Data.Functor.Monadic
@@ -46,7 +50,25 @@ import System.REPL
 
 type DynNode = SuccessorNode SomeException Dynamic
 
+complexDownload :: (Injective a String)
+                => Manager
+                -> Successor SomeException b
+                -> (Request -> Request)
+                -> a
+                -> URL
+                -> ErrorIO' (resColl, localErrs)
+complexDownload = undefined
 
+-- |List- or setlike collections.
+type Collection (c :: (* -> *)) (a :: *) =
+   (Co.Foldable (c a) a,
+    Co.Unfoldable (c a) a,
+    Co.BulkInsertable [a] (c a))
+
+-- |Maplike collections.
+type Maplike (c :: (* -> * -> *)) (k :: *) (a :: *) =
+   (Co.Unfoldable (c k a) (k,a),
+    Co.Map (c k a) k a)
 
 -- |Takes a collection of 'Successor' nodes and tries to download & save
 --  them to disk.
@@ -61,51 +83,71 @@ type DynNode = SuccessorNode SomeException Dynamic
 --    into the set.
 --  * Inner nodes are not handled; they are kept as-is.
 --
---  Errors do not stop the traversal; instead, all occurring errors are collected
-downloadForest :: forall a resColl b.
+--  Errors do not stop the traversal; instead, they are collected and returned
+--  as the second component of the result tuple.
+--  The effect of an error on the nodes are as follows:
+--  * If a node requires no remote IO ('PlainText', 'BinaryData', 'XmlResult',
+--    'Info'), it is kept in the result set unchanged, so that a new save
+--    attempt may be made at a later time.
+--  * If a node does require remote IO ('Blob', 'Failure'), an appropriate
+--    failure node is inserted.
+--
+--  The type signature might look daunting, but it works with most common
+--  data types: @resColl@ can be a list or a set, and @localErrs@ is a map.
+downloadForest :: forall a results b errors.
                   (Injective a String,
-                   Co.Foldable resColl (SuccessorNode SomeException b),
-                   Co.Unfoldable resColl (SuccessorNode SomeException b),
-                   Appendable [SuccessorNode SomeException b] resColl)
+                   Collection results (SuccessorNode SomeException b),
+                   Maplike errors (SuccessorNode SomeException b) SomeException)
                => Manager
                -> Successor SomeException b
                -> (Request -> Request)
                -> a
-               -> resColl
-               -> ErrorIO' resColl
-downloadForest m succ reqMod saveLocation = Co.foldlM collect Co.empty
+               -> results (SuccessorNode SomeException b)
+               -> ErrorIO' (results (SuccessorNode SomeException b), errors (SuccessorNode SomeException b) SomeException)
+downloadForest m succ reqMod saveLocation = Co.foldlM collect (Co.empty,Co.empty)
    where
       -- generic writing function for plain text/XML/binary/info data.
-      --writeValue :: (a -> BL.ByteString) -> URL -> a -> ErrorIO (Maybe )
-      --writeValue = undefined
-      writeValue xs f url v ext =
-         (do uuid <- liftIO nextRandom >$> showT
+      {-writeValue :: (resColl, localErrs)
+                 -> (SuccessorNode SomeException b -> BL.ByteString)
+                 -> URL
+                 -> SuccessorNode SomeException b
+                 -> String
+                 -> ErrorIO (resColl, localErrs)-}
+      writeValue (xs,err) f url v ext =
+         (do uuid <- catchIO (to "Couldn't save data from '" ++ url ++ to "'!")
+                             FileError nextRandom
+                     >$> showT
              saveURL saveLocation (url++ to "_" ++uuid++ to ext) $ f v
-             return xs)
+             return (xs,err))
          `catchError`
-         (\e -> return $ flip Co.insert xs v)
+         (\e -> return (Co.insert v xs, Co.insertWith const v e err))
 
-      --formatInfo :: DynNode -> BL.ByteString
-      formatInfo x = T.encodeUtf8 $ k ++ to "\n" ++ v
-         where (k,v) = infoKey &&& infoValue $ nodeRes x
+      --collect :: (resColl, localErrs) -> SuccessorNode SomeException b
+      --                                -> ErrorIO' (resColl, localErrs)
 
-      collect :: resColl -> SuccessorNode SomeException b -> ErrorIO' resColl
+      -- Complex download
+      -------------------------------------------------------------------------
 
       --re-run fetchTree. The most complex case.
-      collect xs (SuccessorNode st (Failure e True) reqF url) =
+      collect (xs,err) (SuccessorNode st (Failure e True) reqF url) =
          do res <- extractResults $ fetchTree m succ (reqF.reqMod) st url
-            ys <- downloadForest m succ reqMod saveLocation res
-            return $ ys `append` xs
+            (ys,err') <- downloadForest m succ reqMod saveLocation res
+            return (ys `Co.bulkInsert` xs, err `Co.union` err')
+
+      -- Single download
+      -------------------------------------------------------------------------
 
       -- try to download the Blob and insert a failure node in case of error
-      collect xs (SuccessorNode st Blob reqF url) = undefined
-         {-(downloadSave m (reqF.reqMod) saveLocation url >> return xs)
+      collect (xs,err) (SuccessorNode st Blob reqF url) = undefined
+         (downloadSave m (reqF.reqMod) saveLocation url >> return xs)
          `catchError`
-         (\e -> return $ flip Co.insert xs
-                         (SuccessorNode st (Failure e False) reqF url))-}
+         (\e -> let n = (SuccessorNode st (Failure e False) reqF url) in
+                return (Co.insert n xs, Co.insertWith const n e err))
       -- try to re-download (as Blob)
-      collect xs (SuccessorNode st (Failure e False) reqF url) =
-         collect xs (SuccessorNode st Blob reqF url)
+      collect xs s@SuccessorNode{nodeRes=Failure{}} = collect xs s{nodeRes=Blob}
+
+      -- Local saving
+      -------------------------------------------------------------------------
 
       -- save plain text, XML, info, and binary data locally
       collect xs n@(SuccessorNode _ (PlainText _) _ url) =
@@ -116,43 +158,9 @@ downloadForest m succ reqMod saveLocation = Co.foldlM collect Co.empty
          writeValue xs (fromBinary . nodeRes) url n ".bin"
       collect xs n@(SuccessorNode _ (Info _ _) _ url) =
          writeValue xs formatInfo url n ".info"
+         where
+            formatInfo (SuccessorNode{nodeRes=Info k v}) =
+               T.encodeUtf8 $ k ++ to "\n" ++ v
 
       -- otherwise-case: keep other kinds of nodes as-is.
-      collect xs n = return $ Co.insert n xs
-
--- |The class of data structures to which can be appended.
-class Appendable a b where
-   append :: a -> b -> b
-
-instance Ord a => Appendable [a] (Set a) where
-   append xs ys = S.fromList xs `S.union` ys
-
-instance Appendable [a] [a] where
-   append = (L.++)
-
-instance Co.Unfoldable [a] a where
-   insert = (:)
-   empty = []
-   singleton = (:[])
-
-instance Ord a => Co.Unfoldable (S.Set a) a where
-   insert = S.insert
-   empty = S.empty
-   singleton = S.singleton
-
-instance Ord a => Co.Map (S.Set a) a () where
-   delete = S.delete
-   member = S.member
-   union = S.union
-   intersection = S.intersection
-   difference = S.difference
-   isSubset = S.isSubsetOf
-   isProperSubset = S.isProperSubsetOf
-   lookup a s = if S.member a s then Just () else Nothing
-   alter _ _ s = s
-   mapWithKey _ s = s
-   unionWith _ = S.union
-   intersectionWith _ = S.intersection
-   differenceWith _ = error "differenceWith undefined for sets"
-   isSubmapBy = error "isSubmapBy undefined for sets"
-   isProperSubmapBy = error "isProperSubmapBy undefined for sets"
+      collect (xs,err) n = return $ (Co.insert n xs, err)
