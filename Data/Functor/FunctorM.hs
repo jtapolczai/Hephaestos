d8915abb273bfdf110f4db5f908a401ecdb909f7
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2014-12-16 22:08:09 +0100

    Data.Functor.FunctorM now works.

--- Data/Functor/FunctorM.hs
+++ Data/Functor/FunctorM.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 -- |The class of data structures which support a monadic fmap, i.e.
 --  a generalization of 'Control.Monad.mapM'.
 --
@@ -5,23 +8,23 @@
 --  'Data.Functor.FunctorM.fmapM' has type @(a -> m b) -> f a -> m (f b)@.
 module Data.Functor.FunctorM where
 
+import Control.Monad
+import qualified Data.Tree as T
+
 -- |Data structures to which we can transparently apply a monad function.
-class Functor f => FunctorM f m where
+class (Functor f, Functor m) => FunctorM f m where
    -- |Maps every element of a structure to a monadic value and collects
    --  the results while preserving the original structure.
    fmapM :: (a -> m b) -> f a -> m (f b)
    -- |Maps every element of a structure to a monadic value and throws away
    --  the results.
    fmapM_ :: (a -> m b) -> f a -> m ()
+   fmapM_ f = void . fmapM f
 
-instance Monad m => FunctorM [] m where
-   mapM = C.mapM
-   mapM_ = C.mapM_
+instance (Functor m, Monad m) => FunctorM [] m where
+   fmapM = mapM
+   fmapM_ = mapM_
 
-instance Functor f => FunctorM f Identity where
-   mapM f = fmap (fromIdentity.f)
-   mapM_ _ _ = return ()
-
-instance Monad m => FunctorM T.Tree m where
-   mapM f (T.Node v vs) = C.liftM2 T.Node (f v) (mapM f vs)
-   mapM_ f (T.Node v vs) = f v >> mapM_ f vs >> return ()
+instance (Functor m, Monad m) => FunctorM T.Tree m where
+   fmapM f (T.Node v vs) = liftM2 T.Node (f v) (mapM (fmapM f) vs)
+   fmapM_ f (T.Node v vs) = f v >> mapM_ (fmapM f) vs >> return ()
