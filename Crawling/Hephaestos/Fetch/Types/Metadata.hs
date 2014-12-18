e51f49f0bca8251a8037a6ec3bcd964ce7b5fc3c
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2014-12-18 18:51:27 +0100

    Added Fetch.Types.Metadata
    The file contains the types into which the metadata JSON gets decoded
    (decoding function missing).

---
+++ Crawling/Hephaestos/Fetch/Types/Metadata.hs
@@ -0,0 +1,24 @@
+-- |Contains types for metadata that is stores during a fetch process.
+module Crawling.Hephaestos.Fetch.Types.Metadata where
+
+import Data.Text
+
+import Crawling.Hephaestos.Fetch.Types
+import qualified Crawling.Hephaestos.Fetch.Types.Successor as S
+
+-- |A metadata node.
+data MetaNode = InnerNode{metaURL::URL}
+                | Leaf{metaURL::URL, metaFile::Text, metaType::ResultType}
+
+-- |The type of a FetchResult
+data ResultType = Blob | Inner | PlainText | XmlResult | BinaryData | Failure | Info
+
+-- |Gets the type of a FetchResult
+getType :: S.FetchResult e -> ResultType
+getType S.Blob = Blob
+getType S.Inner = Inner
+getType (S.PlainText _) = PlainText
+getType (S.XmlResult _) = XmlResult
+getType (S.BinaryData _) = BinaryData
+getType (S.Failure _ _) = Failure
+getType (S.Info _ _) = Info
