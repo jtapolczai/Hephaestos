8d6fba7d23e7e7da332b48d64390ea444325ee83
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2015-02-16 22:10:07 +0100

    Added TaskCategories to STM.Utils.

--- Control/Concurrent/STM/Utils.hs
+++ Control/Concurrent/STM/Utils.hs
@@ -5,11 +5,20 @@ module Control.Concurrent.STM.Utils (
    addTask,
    removeTask,
    withTaskLimit,
+   -- * Categories of tasks
+   TaskCategories,
+   makeCategories,
+   insertTask,
+   updateTask,
+   transferTask,
    -- *Other functions
    readWholeQueue,
    parMapSTM,
    ) where
 
+import Prelude hiding (max)
+
+import Control.Arrow (first)
 import Control.Concurrent (forkIO)
 import Control.Concurrent.STM.TVar
 import Control.Concurrent.STM.TQueue
@@ -18,7 +27,12 @@ import Control.Monad.Loops (whileJust)
 import Control.Monad.STM
 import Control.Monad.Trans
 import qualified Data.Collections as Co
+import qualified Data.IntMap as IM
 import Data.Functor.Monadic
+import qualified Data.Map as M
+
+-- Task limits
+-------------------------------------------------------------------------------
 
 -- |Abstract type for a limit on the number of concurrent tasks.
 --  This is a semaphore that might have an infinite value (no limit).
@@ -50,6 +64,53 @@ withTaskLimit lock m = do liftIO $ atomically $ addTask lock
                           res <- m
                           liftIO $ atomically $ removeTask lock
                           return res
+
+-- Download slots
+-------------------------------------------------------------------------------
+
+-- |A collection of categories, each of which contains a collection of
+--  @Int@-indexed items.
+newtype TaskCategories k v = TaskCategories (TVar (M.Map k (IM.IntMap v), Int))
+
+-- |Create a new set of empty task categories.
+makeCategories :: Ord cat => [cat] -> STM (TaskCategories cat v)
+makeCategories xs = newTVar (cats,0) >$> TaskCategories
+   where cats = M.fromList $ map (\k -> (k,IM.empty)) xs
+
+-- |Insert a new task into the given category. The new task is given a fresh
+--  Int as a key (fresh w.r.t. to the given 'TaskCategories' argument).
+--  If the category with the given name does not exist, it is created.
+insertTask :: Ord cat => TaskCategories cat v -> cat -> v -> STM Int
+insertTask (TaskCategories t) cat v = do
+   (t',max) <- readTVar t
+   let newCat = IM.fromList [(max, v)]
+       f _ = IM.insert max v
+       max' = max+1
+   writeTVar t $ (M.insertWith f cat newCat t', seq max' max')
+   return max
+
+-- |Updates a task in the given category and with the given key, if it exists.
+--  If the task does not exist, nothing is done.
+updateTask :: Ord cat => TaskCategories cat v -> cat -> Int -> (v -> v) -> STM ()
+updateTask (TaskCategories t) cat k f =
+   modifyTVar' t (first $ M.adjust (IM.adjust f k) cat)
+
+-- |Moves a task from category to another, if it exists. If the task does not
+--  exist, nothing is done.
+transferTask :: Ord cat => TaskCategories cat v
+             -> cat
+             -- ^The old category
+             -> Int
+             -> cat
+             -- ^The new category
+             -> STM ()
+transferTask (TaskCategories t) oldCat key newCat = do
+   (t',max) <- readTVar t
+   case M.lookup oldCat t' >$> IM.updateLookupWithKey (\_ _ -> Nothing) key of
+      Just (Just v, lessMap) ->  let t'' = M.adjust (IM.insert key v) newCat
+                                           $ M.insert oldCat lessMap t'
+                                 in writeTVar t (t'', max)
+      _ -> return ()
 
 -- |Reads all elements of a queue.
 readWholeQueue :: TQueue a -> STM [a]
